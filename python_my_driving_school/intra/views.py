f475067deca1f37a1d7dc675c6726691a28a39e7
ryan-1337 <ryanaouiche@gmail.com>
2020-12-14 17:52:15 +0100

    init project and try to add tailwind

---
+++ python_my_driving_school/intra/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def index(request):
+    return render(request, 'home/index.html')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    return render(request, 'accounts/login.html')

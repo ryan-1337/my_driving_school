20edecf3605b2f1c7b22d8bb6875377e7503e02f
ryan-1337 <ryanaouiche@gmail.com>
2020-12-16 00:49:20 +0100

    create relationship and role and formula

--- python_my_driving_school/intra/admin.py
+++ python_my_driving_school/intra/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 
+from .models import *
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+
+class ForfaitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'hours_paid')
+
+class RdvAdmin(admin.ModelAdmin):
+    list_display = ('user', 'hours', 'date', 'places', 'instructor')
+
+admin.site.register(Forfait, ForfaitAdmin)
+admin.site.register(Rdv, RdvAdmin)
+admin.site.register(Role, RoleAdmin)
+
 

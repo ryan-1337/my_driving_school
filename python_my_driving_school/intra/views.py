80adde171dd33c296b7b2ababf19331733bb788b
ryan-1337 <ryanaouiche@gmail.com>
2020-12-15 17:32:48 +0100

    finished signin and signup and begin logout

--- python_my_driving_school/intra/views.py
+++ python_my_driving_school/intra/views.py
@@ -1,20 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+from .forms import CreateUserForm
 
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Votre compte à était créer ' + user)
+            return redirect('login')
 
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
-    return render(request, 'accounts/login.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Nom d\'utilisateur ou mot de passe incorrect') 
+    context = {}
+    return render(request, 'accounts/login.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')

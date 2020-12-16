b53a26146570a03c359822ad5929043aca649241
ryan-1337 <ryanaouiche@gmail.com>
2020-12-16 01:41:41 +0100

    create gitignore

--- python_my_driving_school/python_my_driving_school/settings.py
+++ python_my_driving_school/python_my_driving_school/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from .secrets import SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wrx&i$ff5xibilk0(^-++@bc$z97pqx8c%^hof+0%eop-+iorr'
+SECRET_KEY = SECRET_KEY
 
 TAILWIND_APP_NAME = 'intra'
 # SECURITY WARNING: don't run with debug turned on in production!
